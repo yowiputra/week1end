fcd383d91e6fe6322672ee3b9d9e7d2eaa957e67
Yohan Wiputra <yowiputra@gmail.com>
2017-07-02 18:50:16 +0000

    changed callback to show source only

--- https_example.js
+++ https_example.js
@@ -8,8 +8,13 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
